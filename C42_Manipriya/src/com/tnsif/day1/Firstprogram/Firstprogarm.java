8fb4197e74bcb5dd859fc2c4071c891fdba1ebf0
manipriya4399 <167060114+manipriya4399@users.noreply.github.com>
2024-09-03 22:15:24 -0700

    commit 1

---
+++ C42_Manipriya/src/com/tnsif/day1/Firstprogram/Firstprogarm.java
@@ -0,0 +1,11 @@
+package com.tnsif.day1.Firstprogram;
+
+public class Firstprogarm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Manipriya");
+         System.out.println( );
+	}
+
+} 
